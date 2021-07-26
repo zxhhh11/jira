dbe04f97d58cdd0b842a74b54613296869d436ae
zxhhh11 <zxh_hh11@163.com>
2021-07-26 14:16:08 +0800

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import React from "react";
+import logo from "./logo.svg";
 
 function App() {
   return (
@@ -16,7 +17,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React vsdvsd
         </a>
       </header>
     </div>
